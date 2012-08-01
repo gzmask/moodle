1d9c34d358201d206e06241524fae95deaf8d9e7
gzmask <gzmask@gmail.com>
2012-08-01 00:55:39 -0600

    minor changes to test the gzproject theme

--- theme/gzproject/config.php
+++ theme/gzproject/config.php
@@ -1,30 +1,27 @@
 <?php
-
-//风格名字
 $THEME->name = 'gzproject';
 $THEME->parents = array('base');
 $THEME->sheets = array(
-    'core',     /** Must come first**/
-    'admin',
-    'blocks',
-    'calendar',
-    'course',
-    'user',
-    'dock',
-    'grade',
-    'message',
-    'modules',
-    'question',
-    'css3'      /** Sets up CSS 3 + browser specific styles **/
+  'core',     /** Must come first**/
+  'admin',
+  'blocks',
+  'calendar',
+  'course',
+  'user',
+  'dock',
+  'grade',
+  'message',
+  'modules',
+  'question',
+  'css3'      /** Sets up CSS 3 + browser specific styles **/
 );
 
 $THEME->enable_dock = true;
 
-//这里定义了哪个类型页面对应的模板文件
 $THEME->layouts = array(
-	'frontpage' => array(
-		'file' => 'frontpage.php',
-		'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre',
-	),
+  'frontpage' => array(
+    'file' => 'frontpage.php',
+    'regions' => array('side-pre', 'side-post'),
+  'defaultregion' => 'side-pre',
+  ),
 );
---
+++ theme/gzproject/db/install.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+function xmldb_theme_gzproject_install() {
+
+    return true;
+}
